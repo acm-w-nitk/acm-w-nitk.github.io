1a8e7c37ae6174e5577e00f7e57ba3b7385afa06
yashkumar0707 <yashkumar0707@gmail.com>
2020-02-01 02:28:00 +0530

    table pagination

--- js/discover.js
+++ js/discover.js
@@ -5,79 +5,81 @@
 // })
 
 
-var rawbase = 'https://raw.githubusercontent.com/';
-var jsonloc = 'acm-w-nitk/acm-w-nitk.github.io/events/opp.json';
-function onRowClick(tableId, callback) {
-  var table = document.getElementById(tableId),
-      rows = table.getElementsByTagName("tr"),
-      i;
-  for (i = 0; i < rows.length; i++) {
-      table.rows[i].onclick = function (row) {
-          return function () {
-              callback(row);
-          };
-      }(table.rows[i]);
-  }
-};
+// var rawbase = 'https://raw.githubusercontent.com/';
+// var jsonloc = 'acm-w-nitk/acm-w-nitk.github.io/events/opp.json';
+// function onRowClick(tableId, callback) {
+//   var table = document.getElementById(tableId),
+//       rows = table.getElementsByTagName("tr"),
+//       i;
+//   for (i = 0; i < rows.length; i++) {
+//       table.rows[i].onclick = function (row) {
+//           return function () {
+//               callback(row);
+//           };
+//       }(table.rows[i]);
+//   }
+// };
 
 
 
-$.getJSON(rawbase + jsonloc, function( data ) {
-  console.log(data.length);
-  var list ='';
-  $.each(data,function(key,value){
-      list += '<tbody>'
-      list += '<tr class ="breakrow" id="breakrow">'
-      list += '<td>' +value.name+'</td>'
-      list += '<td>'+value.organiser +'</td>'
-      list += '<td>'+value.typeOfEvent +'</td>'
-      list += '<div class="drop">'
-      list += '<tr class="datarow" style="display:none;">'
-      list += '<td> Awards Prizes Benefits </td>'
-      list += '<td >' + value.awardsPrizesBenefits +'</td>'
-      list += '<td></td>'
-      list += '</tr>'
-      list += '<tr class="datarow" style="display:none;">'
-      list += '<td> Event Domain Tags </td>'
-      list += '<td >' + value.eventDomaintags +'</td>'
-      list += '<td></td>'
-      list += '</tr>'
-      list += '<tr class="datarow" style="display:none;">'
-      list += '<td> Website </td>'
-      list += '<td > <a href="'+value.website+'">' + value.website +'</td>'
-      list += '<td></td>'
-      list += '</tr>'
-      list += '<tr class="datarow" style="display:none;">'
-      list += '<td> Eligibility </td>'
-      list += '<td >' + value.eligibility +'</td>'
-      list += '<td></td>'
-      list += '</tr>'
-      list += '<tr class="datarow" style="display:none;">'
-      list += '<td> Description </td>'
-      list += '<td >' + value.description +'</td>'
-      list += '<td></td>'
-      list += '</tr>'
-      list += '</div>'
-      list += '</tr>'
-  });
-  $('#tbl').append(list)
+// $.getJSON(rawbase + jsonloc, function( data ) {
+//   console.log(data.length);
+//   var list ='';
+//   $.each(data,function(key,value){
+//       list += '<tbody>'
+//       list += '<tr class ="breakrow" id="breakrow">'
+//       list += '<td>' +value.name+'</td>'
+//       list += '<td>'+value.organiser +'</td>'
+//       list += '<td>'+value.typeOfEvent +'</td>'
+//       list += '<div class="drop">'
+//       list += '<tr class="datarow" style="display:none;">'
+//       list += '<td> Awards Prizes Benefits </td>'
+//       list += '<td >' + value.awardsPrizesBenefits +'</td>'
+//       list += '<td></td>'
+//       list += '</tr>'
+//       list += '<tr class="datarow" style="display:none;">'
+//       list += '<td> Event Domain Tags </td>'
+//       list += '<td >' + value.eventDomaintags +'</td>'
+//       list += '<td></td>'
+//       list += '</tr>'
+//       list += '<tr class="datarow" style="display:none;">'
+//       list += '<td> Website </td>'
+//       list += '<td > <a href="'+value.website+'">' + value.website +'</td>'
+//       list += '<td></td>'
+//       list += '</tr>'
+//       list += '<tr class="datarow" style="display:none;">'
+//       list += '<td> Eligibility </td>'
+//       list += '<td >' + value.eligibility +'</td>'
+//       list += '<td></td>'
+//       list += '</tr>'
+//       list += '<tr class="datarow" style="display:none;">'
+//       list += '<td> Description </td>'
+//       list += '<td >' + value.description +'</td>'
+//       list += '<td></td>'
+//       list += '</tr>'
+//       list += '</div>'
+//       list += '</tr>'
+//   });
+//   $('#tbl').append(list)
 
  
- //do what you want with data
-});
+//  //do what you want with data
+// });
 
-   onRowClick("tbl", function (row){
-    var value = row.getElementsByTagName("td")[0].innerHTML;
-    console.log("value>>", value);
-  });
+//    onRowClick("tbl", function (row){
+//     var value = row.getElementsByTagName("td")[0].innerHTML;
+//     console.log("value>>", value);
+//   });
 
 
 $('#search_input').keyup(function(){
   search_table($(this).val())
 });
-
+$('#myInput').on( 'keyup', function () {
+  table.search( this.value ).draw();
+} );
 function search_table(value){
-  $('#tbl tr.breakrow').each(function(){
+  $('#data-table').each(function(){
     var found='false';
     $(this).each(function(){
       
@@ -125,3 +127,102 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+
+
+  var table;
+$(document).ready(function(){  
+      table = $('#data-table').DataTable({  
+          "ajax"     : "https://raw.githubusercontent.com/yashkumar0707/learngit/master/data.json",  
+          "columns"     :     [  
+                 {     "data"     :     "name"     },  
+               {     "data"     :     "organiser"},  
+               {     "data"     :     "typeOfEvent"},  
+               {     "data"     :     "awardsPrizesBenefits"     },
+               {     "data"     :     "eventDomainTags"     },
+               {     "data"     :     "website"     },
+               {     "data"     :     "applicationDate"     },
+               {     "data"     :     "eligibility"     },
+               {     "data"     :     "description"     }
+          ], 
+          "columnDefs" : [
+
+          ]
+     }); 
+});  
+ 
+function hidden3()
+{
+    console.log(table.column(3).visible())
+ 	if(table.column(3).visible())
+    table.column(3).visible( false );
+    else
+    table.column(3).visible( true );
+}
+function hidden4()
+{
+    console.log(table.column(4).visible())
+ 	if(table.column(4).visible())
+    table.column(4).visible( false );
+    else
+    table.column(4).visible( true );
+}
+function hidden5()
+{
+    console.log(table.column(5).visible())
+ 	if(table.column(5).visible())
+    table.column(5).visible( false );
+    else
+    table.column(5).visible( true );
+}
+function hidden6()
+{
+    console.log(table.column(6).visible())
+ 	if(table.column(6).visible())
+    table.column(6).visible( false );
+    else
+    table.column(6).visible( true );
+}
+function hidden7()
+{
+    console.log(table.column(7).visible())
+ 	if(table.column(7).visible())
+    table.column(7).visible( false );
+    else
+    table.column(7).visible( true );
+}
+function hidden8()
+{
+    console.log(table.column(8).visible())
+ 	if(table.column(8).visible())
+    table.column(8).visible( false );
+    else
+    table.column(8).visible( true );
+}
+
+
+
+function check3()
+{
+    hidden3()
+}
+function check4()
+{  
+    hidden4()
+}
+function check5()
+{  
+    hidden5()
+}
+function check6()
+{ 
+    hidden6()
+}
+function check7()
+{  
+    hidden7()
+}
+function check8()
+{  
+    hidden8()
+}
