81d83e0f0adeefe0bd7e05bcb4746abfc60be441
yashkumar0707 <yashkumar0707@gmail.com>
2020-02-01 21:11:26 +0530

    modified the table

--- js/discover.js
+++ js/discover.js
@@ -301,7 +301,7 @@
 /* Formatting function for row details - modify as you need */
 function format ( d ) {
   // `d` is the original data object for the row
-  return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+  return '<table cellpadding="5" cellspacing="0" border="0" style="width:100%;padding-left:50px;">'+
       '<tr>'+
           '<td>Awards, Prizes, Benifits</td>'+
           '<td>'+d.awardsPrizesBenefits+'</td>'+
