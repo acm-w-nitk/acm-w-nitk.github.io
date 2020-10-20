5986afea9fc467c80291c287b142f2ee41c3c416
ganesh <soccerganesh@gmail.com>
2020-10-20 17:54:10 +0530

    fonts added, card resized, responsive

--- js/upcoming_events.js
+++ js/upcoming_events.js
@@ -6,21 +6,3 @@
     vertical: true,
     horizontal: false
   });
-
-  var rellax = new Rellax('.rellax-events', {
-    speed: -0.5,
-    center: false,
-    wrapper: null,
-    round: true,
-    vertical: true,
-    horizontal: false
-  });
-
-  var rellax = new Rellax('.rellax-events-odd', {
-    speed: 0.5,
-    center: false,
-    wrapper: null,
-    round: true,
-    vertical: true,
-    horizontal: false
-  });
