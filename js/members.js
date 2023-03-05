e272d992ea67407b4774c2a446c2c6457e5c2647
Krishna Kumar K <krishnakumar72480@gmail.com>
2023-03-05 21:20:04 +0530

    basic structure completed

--- js/members.js
+++ js/members.js
@@ -3,7 +3,7 @@ const members1 = [
     id: 1,
     name: "Supritha Harishankar",
     desc: "Chairperson",
-    photo: "images/Supritha.png",
+    photo: "../images/Supritha.png",
     facebook: "https://www.facebook.com/acmwnitk/",
     linkedin: "https://www.linkedin.com/in/suprithahs/"
   },
@@ -11,7 +11,7 @@ const members1 = [
     id: 2,
     name: "Subham Chakraborty",
     desc: "Vice-Chair",
-    photo: "images/subham.jpg",
+    photo: "../images/subham.jpg",
     facebook: "https://www.facebook.com/acmwnitk/",
     linkedin: "https://www.linkedin.com/in/subham-chakraborty-653226196/",
   },
@@ -19,7 +19,7 @@ const members1 = [
     id: 3,
     name: "Vageesha Mishra",
     desc: "Content Head",
-    photo: "images/Vageesha.png",
+    photo: "../images/Vageesha.png",
     linkedin: "https://www.linkedin.com/in/vageesha-mishra-8288a419a",
     facebook: "https://www.facebook.com/vageesha.mishraa"
   }
@@ -29,7 +29,7 @@ const members2 = [
     id: 4,
     name: "Meghna Kashyap",
     desc: "Publicity Coordinator",
-    photo: "images/Meghna.jpg",
+    photo: "../images/Meghna.jpg",
     linkedin: "https://www.linkedin.com/in/meghna-kashyap-4a6860196/",
     facebook: "https://www.facebook.com/meghna.kashyap.98/"
   },
@@ -38,7 +38,7 @@ const members2 = [
     id: 5,
     name: "Yash Kumar Gupta",
     desc: "Website Head",
-    photo: "images/yash.jpg",
+    photo: "../images/yash.jpg",
     linkedin: "https://www.linkedin.com/in/yash-kumar-gupta-41a931143",
     facebook: "https://www.facebook.com/yash.gupta.353803"
   }
